c69b77792d81c61b2ef21cacaa25f88e5f0106ef
thecolombianabortion <julianolomanolox@hotmail.com>
2022-03-22 23:29:59 -0500

    clear board function done

--- backend.js
+++ backend.js
@@ -1,12 +1,12 @@
-function generate_board(size){
-    const father = document.querySelector('#container');
+const father = document.querySelector('#container');
 
+function generate_board(size){
     for(let i = 1; i <=size;i++){
         const element = document.createElement('div');
         element.setAttribute('id',`grid-item${i}`);
         element.classList.add('grid-item');
         element.textContent= i;
-        father.append(element);
+        father.appendChild(element);
     }
 }
 generate_board(256);
@@ -15,15 +15,21 @@ generate_board(256);
 const divs = document.querySelectorAll('div');
 
 let mouseENCIMA = (e) =>{
+    if(!(e.target.id == "container")){
     const divcito = document.querySelector('#'+e.target.id);
     divcito.classList.add('grid-item-blue');
+    }
 };
 
 divs.forEach(div => {
     div.addEventListener('mouseover',mouseENCIMA);
 });
 
-
 function clear_the_board(){
+    const divs2 = document.querySelectorAll('.grid-item');
+    divs2.forEach(div=>{
+        father.removeChild(div);
+        // father.removeChild(div);
+    });
     
 }
