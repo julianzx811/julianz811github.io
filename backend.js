bd359b1de0b1b27ab664462eb3ed7cad9e4f3acb
thecolombianabortion <julianolomanolox@hotmail.com>
2022-03-22 22:57:22 -0500

    pencil wokking fine, the grid needs to be better

--- backend.js
+++ backend.js
@@ -1,23 +1,29 @@
-// const container = document.getElementById("container");
+function generate_board(size){
+    const father = document.querySelector('#container');
 
-// function makeRows(rows, cols) {
-//   container.style.setProperty('--grid-rows', rows);
-//   container.style.setProperty('--grid-cols', cols);
-//   for (c = 0; c < (rows * cols); c++) {
-//     let cell = document.createElement("div");
-//     cell.innerText = (c + 1);
-//     cell.style.borderColor = 'red';
-//     container.appendChild(cell).className = "grid-item";
-//   };
-// };
+    for(let i = 1; i <=size;i++){
+        const element = document.createElement('div');
+        element.setAttribute('id',`grid-item${i}`);
+        element.classList.add('grid-item');
+        element.textContent= i;
+        father.append(element);
+    }
+}
+generate_board(256);
 
-// makeRows(16, 16);
+//pencial draw functions
+const divs = document.querySelectorAll('div');
 
-const father = document.querySelector('#container');
+let mouseENCIMA = (e) =>{
+    const divcito = document.querySelector('#'+e.target.id);
+    divcito.classList.add('grid-item-blue');
+};
 
-for(let i = 0; i <(16*16);i++){
-    const element = document.createElement('div');
-    element.classList.add('grid-item');
-    element.textContent= i;
-    father.append(element);
+divs.forEach(div => {
+    div.addEventListener('mouseover',mouseENCIMA);
+});
+
+
+function clear_the_board(){
+    
 }
